f30516c32c4732753337bfeb054a51c4f3e5b12b
iharuya <i4haruya@gmail.com>
2023-09-26 17:35:25 +0900

    add version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,16 @@
 	import Title from './Title.svelte';
 	import ServerAddress from './ServerAddress.svelte';
 	import Map from './Map.svelte';
+	import McVersion from './MCVersion.svelte';
 </script>
 
 <section class="">
 	<Title />
 	<ServerAddress />
-	<div class="pt-8 pb-8">
+	<div class="flex justify-center mt-6">
+		<McVersion />
+	</div>
+	<div class="mt-8 mb-8">
 		<Map />
 	</div>
 </section>
---
+++ src/routes/MCVersion.svelte
@@ -0,0 +1,8 @@
+<div class="text-center text-white inline-block">
+	<div class="bg-[#418C1E] 0 py-1 px-2 text-lg font-semibold">
+		<span>Minecraft Version</span>
+	</div>
+	<div class="bg-[#62371F] py-3">
+		<span class="text-2xl font-bold">1.20.1</span>
+	</div>
+</div>
